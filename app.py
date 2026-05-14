e73df9d221a2bd45a0c7780506e845f2b9f9d99a
Shreeyansh Jain <shreeyanshj@fpt.com>
2026-05-14 13:43:28 +0530

    fixed the deployment issue

--- app.py
+++ app.py
@@ -1334,4 +1334,8 @@ def parent_teachers():
     )
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(
+        host='0.0.0.0',
+        port=int(os.environ.get("PORT", 8080)),
+        debug=False
+    )
